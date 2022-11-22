23363554dbacdcfddcbd99318584c8cab8e9b717
Soni-Aman <106104444+Soni-Aman@users.noreply.github.com>
2022-11-22 14:37:30 +0530

    Add files via upload

---
+++ testAddStrings.java
@@ -0,0 +1,15 @@
+package jUnitTestPackage;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class testAddStrings {
+
+			@Test
+		public void test() {
+			jUnitFunctions junitstring = new jUnitFunctions();
+			String res = junitstring.AddStrings("capstone", "project");
+			assertEquals("capstoneproject", res);
+		}
+	}
